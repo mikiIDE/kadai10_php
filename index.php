0c70aeb78caddf235a3b3b7cb6d14ca74c740ee3
mikiIDE <kinoko.miki@gmail.com>
2024-11-22 23:30:20 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CODE ∞ LINK</title>
+</head>
+
+<body>
+
+    <!-- ヘッダー -->
+    <?php
+    include("inc/header.html");
+    ?>
+
+    <!-- メインページ -->
+<p class="greeting">
+    コードリンクへようこそ！<br>
+    先人たちの知恵を、努力を、みんなで享受し<br>
+    新たにセカイを変えていきましょう！
+</p>
+<p class="guide">
+    コードの紹介をしたい方は「share」<br>
+    コードを見たい方は「read」へ進んでね
+</p>
+
+<!-- フッター -->
+ <div class="footer">
+ ©CODE LINK 2024 All rights reserved.
+ </div>
+</body>
+
+</html>
