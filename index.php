4d8dbf56e05e20a4444d2e5e8688ea92abaa0129
mikiIDE <kinoko.miki@gmail.com>
2024-11-23 00:07:42 +0900

    メインレイアウト調整

--- index.php
+++ index.php
@@ -1,34 +1,27 @@
+<!-- index.php -->
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
     <title>CODE ∞ LINK</title>
 </head>
-
 <body>
-
-    <!-- ヘッダー -->
-    <?php
-    include("inc/header.html");
-    ?>
-
-    <!-- メインページ -->
-<p class="greeting">
-    コードリンクへようこそ！<br>
-    先人たちの知恵を、努力を、みんなで享受し<br>
-    新たにセカイを変えていきましょう！
-</p>
-<p class="guide">
-    コードの紹介をしたい方は「share」<br>
-    コードを見たい方は「read」へ進んでね
-</p>
-
-<!-- フッター -->
- <div class="footer">
- ©CODE LINK 2024 All rights reserved.
- </div>
+    <?php include("inc/header.html"); ?>
+    <main>
+        <p class="greeting">
+            コードリンクへようこそ！<br>
+            先人たちの知恵を、努力を、みんなで享受し<br>
+            新たにセカイを変えていきましょう！
+        </p>
+        <p class="guide">
+            コードの紹介をしたい方は「share」<br>
+            コードを見たい方は「read」へ進んでね
+        </p>
+    </main>
+    <footer class="footer">
+        ©CODE LINK 2024 All rights reserved.
+    </footer>
 </body>
-
 </html>
